87a49e6048446b670de69cd03b080a9fd0928243
Takuya UESHIN <ueshin@happy-camper.st>
2012-09-16 21:42:11 +0900

    Initial commit.

---
+++ scaladocs/index.js
@@ -0,0 +1,1 @@
+Index.PACKAGES = {"st" : [], "st.happy_camper.maven.plugins.ensime" : [{"class" : "st\/happy_camper\/maven\/plugins\/ensime\/EnsimeGenerateMojo.html", "name" : "st.happy_camper.maven.plugins.ensime.EnsimeGenerateMojo"}], "st.happy_camper.maven" : [], "st.happy_camper" : [], "st.happy_camper.maven.plugins" : []};
